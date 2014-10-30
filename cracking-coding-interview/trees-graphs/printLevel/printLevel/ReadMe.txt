9fcf56c38c3af12038109667949b2e0cb4024d1c
psvishnu91 <psvishnu.91@gmail.com>
2014-10-30 12:23:40 -0400

    added printing levelwise for trees

---
+++ cracking-coding-interview/trees-graphs/printLevel/printLevel/ReadMe.txt
@@ -0,0 +1,40 @@
+========================================================================
+    CONSOLE APPLICATION : printLevel Project Overview
+========================================================================
+
+AppWizard has created this printLevel application for you.
+
+This file contains a summary of what you will find in each of the files that
+make up your printLevel application.
+
+
+printLevel.vcxproj
+    This is the main project file for VC++ projects generated using an Application Wizard.
+    It contains information about the version of Visual C++ that generated the file, and
+    information about the platforms, configurations, and project features selected with the
+    Application Wizard.
+
+printLevel.vcxproj.filters
+    This is the filters file for VC++ projects generated using an Application Wizard. 
+    It contains information about the association between the files in your project 
+    and the filters. This association is used in the IDE to show grouping of files with
+    similar extensions under a specific node (for e.g. ".cpp" files are associated with the
+    "Source Files" filter).
+
+printLevel.cpp
+    This is the main application source file.
+
+/////////////////////////////////////////////////////////////////////////////
+Other standard files:
+
+StdAfx.h, StdAfx.cpp
+    These files are used to build a precompiled header (PCH) file
+    named printLevel.pch and a precompiled types file named StdAfx.obj.
+
+/////////////////////////////////////////////////////////////////////////////
+Other notes:
+
+AppWizard uses "TODO:" comments to indicate parts of the source code you
+should add to or customize.
+
+/////////////////////////////////////////////////////////////////////////////
---
+++ cracking-coding-interview/trees-graphs/printLevel/printLevel/printLevel.cpp
@@ -0,0 +1,195 @@
+// printHorizontalLevel.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+#include <vector>
+#include <list>
+#include <stddef.h>
+#include <math.h>
+
+using namespace std;
+
+
+//-----------------------------------------------------------------------
+// Node of tree
+template <typename T>
+struct node
+{
+	node *l , *r;
+	T data;
+};
+
+//-----------------------------------------------------------------------
+
+// Create a node of a binary tree
+template <typename T>
+node<T>* createNode(T a)
+{
+	node<T>* t = new node<T>;	
+	t->data = a;
+	t->l = NULL;
+	t->r = NULL;
+	return t;
+}
+
+// Build a binary tree
+template <typename T>
+node<T>* buildTree()
+{
+	node<T>* root = createNode<T>(1);
+	root->l = createNode<T>(2);
+	root->r = createNode<T>(3);
+	root->l->l = createNode<T>(4);
+	root->l->r = createNode<T>(5);
+	root->l->l->l = createNode<T>(6);
+	return root;
+}
+
+
+//-----------------------------------------------------------------------
+/*
+Print tree each horizontal level at a time recursion function
+*/
+template <typename T>
+void printHorizontalLevel(node<T>* t)
+{
+	vector < list <node<T>*> > levelLists;
+	printHorizontalLevel(t, levelLists, 0);
+
+	for(size_t i = 0; i < levelLists.size() ; i++)
+	{
+		for( auto it = levelLists[i].begin(); it != levelLists[i].end(); it++)
+		{
+			node<T>* currNode = *it;
+			cout<<currNode->data <<"  ";
+		}
+		cout<<"\n";
+	}
+}
+
+/*
+Print tree each horizontal level at a time recursion function
+*/
+template <typename T>
+void printHorizontalLevel( node<T>* t, vector< list< node<T>* > > &levelLists, int level )
+{
+	if(t==NULL) return;
+
+	if ((int)levelLists.size() > level)
+	{
+		levelLists[level].push_back(t);
+	}
+	else
+	{
+		list<node<T>*> nextLevel;
+		nextLevel.push_back(t);
+		levelLists.push_back(nextLevel);
+	}
+	printHorizontalLevel(t->l , levelLists, level+1);
+	printHorizontalLevel(t->r , levelLists, level+1);
+}
+
+
+//-----------------------------------------------------------------------
+
+/*
+Print tree each vertical level at a time wrapper function
+*/
+template <typename T>
+void printVerticalLevel(node<T> * t)
+{
+	vector < list <node<T>*> > levelListsRight;
+	vector < list <node<T>*> > levelListsLeft;
+	
+	printVerticalLevel(t, levelListsLeft, levelListsRight, 0);
+
+	
+	for(int i = (int)levelListsLeft.size()-1; i>=0 ; i--)
+	{
+		for( auto it = levelListsLeft[i].begin(); it != levelListsLeft[i].end(); it++)
+		{
+			node<T>* currNode = *it;
+			cout<<currNode->data <<"  ";
+		}
+		cout<<"\n";
+	}
+
+	for(size_t i = 0; i < levelListsRight.size() ; i++)
+	{
+		for( auto it = levelListsRight[i].begin(); it != levelListsRight[i].end(); it++)
+		{
+			node<T>* currNode = *it;
+			cout<<currNode->data <<"  ";
+		}
+		cout<<"\n";
+	}
+}
+
+
+/*
+Print tree each vertical level at a time recursion function
+*/
+template <typename T>
+void printVerticalLevel(node<T> *t, vector < list <node<T>*> > &leftList, vector< list< node<T>* > > &rightList, int pos)
+{
+	if (t==NULL) return;
+
+	if(pos>=0)
+	{
+		if((int)rightList.size()>pos)
+		{
+			rightList[pos].push_back(t);
+		}
+		else
+		{
+			list<node<T>*> newList;
+			newList.push_back(t);
+			rightList.push_back(newList);
+		}
+	}
+	else
+	{
+		int absPos = abs(pos)-1;
+		if((int)leftList.size()>absPos)
+		{
+			leftList[absPos].push_back(t);
+		}
+		else
+		{
+			list<node<T>*> newList;
+			newList.push_back(t);
+			leftList.push_back(newList);
+		}
+
+	}
+	printVerticalLevel(t->l, leftList, rightList, pos-1);
+	printVerticalLevel(t->r, leftList, rightList, pos+1);
+}
+
+
+
+
+//-----------------------------------------------------------------------
+
+void main()
+{
+	//Build tree
+	node<int> *t = buildTree<int>();
+	
+	// Print the tree each horizontal level in a line
+	cout<<"Printing horizontal Level : \n";
+	printHorizontalLevel(t);
+	
+	
+	// Print the tree each vertical level in a line
+	cout<<"Printing Vertical Level : \n";
+	printVerticalLevel(t);
+	
+	char c;
+	cout<<"Enter any character : ";
+	cin>>c;
+}
+
+//-----------------------------------------------------------------------
+
