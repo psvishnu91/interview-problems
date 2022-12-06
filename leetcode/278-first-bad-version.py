9d495ac4616ec7569de9382ecb58a75b9a826c94
Vishnu Sreenivasan <psvishnu91@users.noreply.github.com>
2022-12-06 14:10:03 -0500

    Create 278-first-bad-version.py

---
+++ leetcode/278-first-bad-version.py
@@ -0,0 +1,36 @@
+"""278. First Bad Version
+
+https://leetcode.com/problems/first-bad-version/description/
+
+Test
+====
+# G G G G B B B B B B
+# 0 1 2 3 4 5 6 7 8 9
+# l=0, r=9, m=4, B
+# l=0, r=4, m=2, G
+# l=2, r=4, m=3, G
+# l=4, r=4, m=3, B
+# l=5, r=5, m=5, B
+# B B B
+# 0 1 2
+# l=0,rt=2,m=1
+# l=0,rt=1,m=0,
+# l=0, rt=0
+# B
+# l=0,rt=0,m=0
+"""
+class Solution:
+    def firstBadVersion(self, n: int) -> int:
+        lt = 1
+        rt = n
+        while (lt < rt):
+            mid = (lt + rt) // 2
+            if isBadVersion(mid):
+                rt=mid
+            else:
+                lt=mid+1
+        # lt==rt, unit element
+        # First unit element is the first element to switch
+        return lt
+
+            
